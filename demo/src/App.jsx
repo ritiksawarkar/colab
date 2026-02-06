ff6743e2bc08e69c43f28c928f01c1010ab4238f
luckyghangare <luckyghangare@gmail.com>
2026-02-06 15:27:48 +0530

    Added commit

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,13 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Contact />;
 }
 export default App;
---
+++ demo/src/pages/Contact.jsx
@@ -0,0 +1,6 @@
+const Contact = () => (
+  <div>
+    <h1>Contact Page</h1>
+  </div>
+);
+export default Contact;
