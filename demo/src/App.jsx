db8405139f3d6385a1899d36e2238ba44b5c31bf
Ritik Sawarkar <ritiksawarkar027@gmail.com>
2026-02-06 15:45:59 +0530

    Fix routing for about and contact pages

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,6 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
 import Contact from "./pages/Contact";
 
 function App() {
-  return <Contact />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* optional */}
+        <Route path="/" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
+
 export default App;
